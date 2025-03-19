25937b7dca115e285ae20e084acd09fae4b3cbf5
BrilantPonxhaj1 <ponxhaj@pupil.ch>
2025-03-20 00:29:27 +0100

    remove unnecessary comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,43 +28,36 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    // Getter for the first_name attribute.
     public function getFirstName(): ?string
     {
         return $this->attributes['first_name'] ?? null;
     }
 
-    // Getter for the last_name attribute.
     public function getLastName(): ?string
     {
         return $this->attributes['last_name'] ?? null;
     }
 
-    // Getter for the username attribute.
     public function getUsername(): ?string
     {
         return $this->attributes['username'] ?? null;
     }
 
-    // Getter for the email attribute.
     public function getEmail(): ?string
     {
         return $this->attributes['email'] ?? null;
     }
 
-    // Getter for the phone attribute.
     public function getPhone(): ?string
     {
         return $this->attributes['phone'] ?? null;
     }
 
-    // Getter for the hashed password.
     public function getPassword(): ?string
     {
         return $this->attributes['password'] ?? null;
     }
 
-    // Getter for the type attribute.
     public function getType(): ?string
     {
         return $this->attributes['type'] ?? null;
