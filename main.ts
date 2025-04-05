b7ed2e78c9a4996daea3a723d1ad898fafcf2272
Afrim Ymeri <78933251+afrimymeri@users.noreply.github.com>
2025-04-05 20:16:39 +0200

    Template-UI
    Added template ui for admin-dashbaoard

--- main.ts
+++ main.ts
@@ -3,7 +3,19 @@ import App from './App.vue'
 import router from './js/lib/router'
 import axios from 'axios'
 
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives
+})
+
 const app = createApp(App)
 axios.defaults.baseURL = 'http://localhost:8000/api'
 app.use(router)
+app.use(vuetify)
 app.mount('#app')
